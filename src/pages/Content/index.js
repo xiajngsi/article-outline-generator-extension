5b577d57e50ef2faa7a3ec0a4b016ced9ed6383a
LX@layer2 <shanelau1021@gmail.com>
2022-08-19 16:11:48 +0800

    merge request

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -12,15 +12,15 @@ console.log('Must reload extension for modifications to take effect.');
 printLine("Using the 'printLine' function from the Print Module");
 
 
-// 构造位置数据
-const sectionsDom = document.querySelectorAll('h2')
 const settings = {
         sections: 'h2',
         // insertTarget: elem,
         insertLocation: 'before',
         easingStyle: 'easeOutQuad',
         updateHistory: true
-    }
+}
+// 构造位置数据
+const sectionsDom = document.querySelectorAll('h2')
 const h2List = populateSectionData(sectionsDom, settings)
 
 const callData = {
